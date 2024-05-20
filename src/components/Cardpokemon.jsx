48e01d67703132a016d65e130e77f024398b3597
Eugenia Olmedo <eugeniaolmedo@gmail.com>
2024-05-20 18:32:44 -0300

    Cambio estilo cards

--- src/components/Cardpokemon.jsx
+++ src/components/Cardpokemon.jsx
@@ -1,7 +1,8 @@
-import {Row,Col,Card,CardBody,CardFooter,CardImg,Badge} from 'reactstrap'
+// import {Col,Card,CardBody,CardFooter,CardImg,Badge} from 'reactstrap'
 import {useState,useEffect} from 'react'
 import { Link } from 'react-router-dom';
 import axios from 'axios'
+import './Cardpokemon.css'
 
 
 export const CardPokemon = (params) => {
@@ -24,18 +25,37 @@ export const CardPokemon = (params) => {
   }
 
   return (
-    <Col sm='4' lg='3' className='mb-3'>
-      <Card className={'card-hover shadow border-4 border-danger'+cardClass}>
-        <CardImg src={imagen} height='270' className='p-2' />
-        <CardBody className='text-center'>
-          <Badge pill color='danger'># {pokemon.id} </Badge>
-          <label className='text-capitalize fw-bold'>{pokemon.name}</label>
-        </CardBody>
-        <CardFooter className='bg-danger'>
-          <Link to={'/pokemon/'+pokemon.name} className='btn btn-dark'>Información</Link>
-        </CardFooter>
-      </Card>
-    </Col>
+  <>
+    <ul className='cardGrid'>
+    <div className='cardFrame'>
+      <div>
+        <span className='cardText'>#{pokemon.id} </span>
+        <span className='cardTitle'>{pokemon.name}</span>
+      
+      </div>
+      <div className='cardImage'>
+        <img src={imagen} alt="pokemon"/>
+      </div>
+      {/* <a className='cardText' src={'/pokemon/'+pokemon.name}>Información</a> */}
+      <button className='cardButton' src={'/pokemon/'+pokemon.name}>Información</button>
+
+    </div>
+    </ul> 
+  </>
+
+
+    // <Col sm='4' lg='3' className='mb-3'>
+    //   <Card className={'card-hover shadow border-4 border-danger'+cardClass}>
+    //     <CardImg src={imagen} height='270' className='p-2' />
+    //     <CardBody className='text-center'>
+    //       <Badge pill color='danger'># {pokemon.id} </Badge>
+    //       <label className='text-capitalize fw-bold'>{pokemon.name}</label>
+    //     </CardBody>
+    //     <CardFooter className='bg-danger'>
+    //       <Link to={'/pokemon/'+pokemon.name} className='btn btn-dark'>Información</Link>
+    //     </CardFooter>
+    //   </Card>
+    // </Col>
   )
 }
 
